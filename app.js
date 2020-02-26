dd58a348be1105faff1abf4c94847147fea5c4b4
Cristobal Heis <cristobal.heiss.j@gmail.com>
2020-02-26 13:48:35 +0100

    added cors option

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3000;
 const pickRandomQuote = require("./src/pickRandomQuote");
 const lotrQuotes = require("./src/quotes");
 
+const cors = require('cors');
+app.use(cors({optionSuccessStatus: 200})); 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
