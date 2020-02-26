dd4178abb6a897d224936424068790d0e9f430a2
Cristobal Heis <cristobal.heiss.j@gmail.com>
2020-02-26 15:50:14 +0100

    added package for security

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 const pickRandomQuote = require("./src/pickRandomQuote");
 const lotrQuotes = require("./src/quotes");
+const rateLimit = require("express-rate-limit");
+ 
+// Has to be enabled since app runs on Heroku
+app.set('trust proxy', 1);
+ 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+ 
+//  apply to all requests
+app.use(limiter);
 
 const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200})); 
