fb3c4e3b59f6ee2d814ce5e12742106d5ac93b52
Cristobal Heis <cristobal.heiss.j@gmail.com>
2020-02-26 09:46:21 +0100

    fixed a typo

--- src/quotes.js
+++ src/quotes.js
@@ -4,107 +4,107 @@ const lotrQuotes = [
   {
     quote: "It’s the job that’s never started as takes longest to finish.",
     author: "Sam Gamgee",
-    image: `${hotUrl}sam.jpg`
+    image: `${hostUrl}sam.jpg`
   },
   {
     quote: "But in the end it’s only a passing thing, this shadow; even darkness must pass.",
     author: "Sam Gamgee",
-    image: `${hotUrl}sam.jpg`
+    image: `${hostUrl}sam.jpg`
   },
   {
     quote: "It is not the strength of the body, but the strength of the spirit.",
     author: "J.R.R. Tolkien",
-    image: `${hotUrl}tolkien.jpg`
+    image: `${hostUrl}tolkien.jpg`
   },
   {
     quote: "It is useless to meet revenge with revenge: it will heal nothing.",
     author: "Frodo Baggins",
-    image: `${hotUrl}frodo.jpg`
+    image: `${hostUrl}frodo.jpg`
   },
   {
     quote: "Deeds will not be less valiant because they are unpraised.",
     author: "Aragorn",
-    image: `${hotUrl}aragorn.jpg`
+    image: `${hostUrl}aragorn.jpg`
   },
   {
     quote: "Even the smallest person can change the course of history.",
     author: "Lady Galadriel",
-    image: `${hotUrl}galadriel.jpg`
+    image: `${hostUrl}galadriel.jpg`
   },
   {
     quote: "There’s some good in this world, Mr. Frodo… and it’s worth fighting for.",
     author: "Sam Gamgee",
-    image: `${hotUrl}sam.jpg`
+    image: `${hostUrl}sam.jpg`
   },
   {
     quote: "A hunted man sometimes wearies of distrust and longs for friendship.",
     author: "Aragorn",
-    image: `${hotUrl}aragorn.jpg`
+    image: `${hostUrl}aragorn.jpg`
   },
   {
     quote: "Yet such is oft the course of deeds that move the wheels of the world: small hands do them because they must, while the eyes of the great are elsewhere.",
     author: "Elrond",
-    image: `${hotUrl}elrond.jpg`
+    image: `${hostUrl}elrond.jpg`
   },
   {
     quote: "It is a strange fate that we should suffer so much fear and doubt over so small a thing… such a little thing.",
     author: "Boromir",
-    image: `${hotUrl}boromir.jpg`
+    image: `${hostUrl}boromir.jpg`
   },
   {
     quote: "He that breaks a thing to find out what it is, has left the path of wisdom.",
     author: "Gandalf",
-    image: `${hotUrl}gandalf.jpg`
+    image: `${hostUrl}gandalf.jpg`
   },
   {
     quote: "Many that live deserve death. And some that die deserve life. Can you give it to them? Then do not be eager to deal out death in judgement. For even the very wise cannot see all ends.",
     author: "Gandalf",
-    image: `${hotUrl}gandalf.jpg`
+    image: `${hostUrl}gandalf.jpg`
   },
   {
     quote: "It is not our part to master all the tides of the world, but to do what is in us for the succor of those years wherein we are set, uprooting the evil in the fields that we know, so that those who live after may have clean earth to till.",
     author: "Gandalf",
-    image: `${hotUrl}gandalf.jpg`
+    image: `${hostUrl}gandalf.jpg`
   },
   {
     quote: "Many are the strange chances of the world, and help oft shall come from the hands of the weak when the wise falter.",
     author: "Gandalf",
-    image: `${hotUrl}gandalf.jpg`
+    image: `${hostUrl}gandalf.jpg`
   },
   {
     quote: "The burned hand teaches best. After that advice about fire goes to the heart.",
     author: "Gandalf",
-    image: `${hotUrl}gandalf.jpg`
+    image: `${hostUrl}gandalf.jpg`
   },
   {
     quote: "Faithless is he that says farewell when the road darkens.",
     author: "Gimli",
-    image: `${hotUrl}gimli.jpg`
+    image: `${hostUrl}gimli.jpg`
   },
   {
     quote: "Torment in the dark was the danger that I feared, and it did not hold me back.",
     author: "Gimli",
-    image: `${hotUrl}gimli.jpg`
+    image: `${hostUrl}gimli.jpg`
   },
   {
     quote: "Who knows? Have patience. Go where you must go, and hope!",
     author: "Gandalf",
-    image: `${hotUrl}gandalf.jpg`
+    image: `${hostUrl}gandalf.jpg`
   },
   {
     quote: "It is not despair, for despair is only for those who see the end beyond all doubt. We do not!",
     author: "Gandalf",
-    image: `${hotUrl}gandalf.jpg`
+    image: `${hostUrl}gandalf.jpg`
   },
   {
     quote: "Maybe the paths that you each shall tread are already laid before your feet, though you do not see them.",
     author: "Lady Galadriel",
-    image: `${hotUrl}galadriel.jpg`
+    image: `${hostUrl}galadriel.jpg`
   },
   {
     quote: "Memory is not what the heart desires. That is only a mirror.",
     author: "Gimli",
-    image: `${hotUrl}gimli.jpg`
+    image: `${hostUrl}gimli.jpg`
   },
 ]
 
